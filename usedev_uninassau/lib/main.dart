9ef25ee649330553071c4279ca095f406b9d0258
carloscesarcontact <Carloscesar.contact@gmail.com>
2026-05-29 08:37:56 -0300

    feat: configura rotas e tema global do app

--- usedev_uninassau/lib/main.dart
+++ usedev_uninassau/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:usedev_uninassau/src/screens/initial_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'src/screens/initial_screen.dart';
+import 'src/screens/product_detail_screen.dart';
+import 'src/screens/cart_screen.dart';
+import 'src/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +11,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'UseDev',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: InitialScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF780BF7)),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const InitialScreen(),
+        '/product': (context) => const ProductDetailScreen(),
+        '/cart': (context) => CartScreen(),
+        '/login': (context) => const LoginScreen(),
+      },
     );
   }
 }
